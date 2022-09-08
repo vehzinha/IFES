329d4db80aec506958b2cc7e7e77990d9a3509a1
vehzinha <101737211+vehzinha@users.noreply.github.com>
2022-09-08 20:37:42 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,100 +1,93 @@
-const tamanhoGeral = 70
-const tamanhoTable = 8
-let pId = 0
-let getId
-f_criarTabela()
+const tamanhoCelula = 40;
+let pecaId = 0;
+let lf = 0;
+let la = 0;
+let c = "";
+document.body.append(criaTabuleiro());
 
-function f_criarTabela() {
-  let tabela = document.createElement('table')
-  tabela.style.border = '5px solid black'
-  tabela.style.borderCollapse = 'collapse'
-  tabela.style.margin = '0 auto'
-  
-  for (let l = 0; l < tamanhoTable; l++) {
-    let linha = document.createElement('tr')
-    
-    for (let c = 0; c < tamanhoTable; c++) {
-      let casa = document.createElement('td')
-      casa.dataset.lin = l
-      casa.dataset.col = c
-      casa.classList.add('casa')
-      
-      if (l % 2 == c % 2) {
-        casa.style.backgroundColor = 'black'
-        casa.addEventListener('dragover', f_over)
-        
-        if (l * 8 + c < 24) {
-          const imagem = f_criarPeca('black')
-          imagem.setAttribute('draggable', 'False')
-          casa.append(imagem)
-          
-        } else if (l * 8 + c >= 40) {
-          
-          const imagem = f_criarPeca('red')
-          casa.append(imagem)
+function criaTabuleiro() {
+    const tamanho = 8;
+    let tabela = document.createElement('table');
+
+    tabela.style.borderStyle = 'solid';
+    tabela.style.borderSpacing = 0;
+    tabela.style.margin = 'auto';
+
+    for (let i = 0; i < tamanho; i++) {
+        let linha = document.createElement('tr');
+		
+        tabela.append(linha);
+        for (let j = 0; j < tamanho; j++) {
+            let celula = document.createElement('td');
+			celula.setAttribute('id', i);
+            linha.append(celula);
+            celula.style.width = `${tamanhoCelula}px`;
+            celula.style.height = `${tamanhoCelula}px`;
+			pecaId += 1;
+            if (i % 2 == j % 2) {
+                celula.style.backgroundColor = 'black';
+				celula.setAttribute("class","droptarget");
+                if (i * 8 + j <= 24) {
+                    celula.append(criaPeca('black',pecaId));
+                } else if (i * 8 + j >= 40) {
+                    celula.append(criaPeca('red',pecaId));
+                }
+            } else {
+                celula.style.backgroundColor = 'white';
+            }
         }
-      } else {
-        casa.style.backgroundColor = 'white'
-      }
-      casa.style.width = `${tamanhoGeral}px`
-      casa.style.height = `${tamanhoGeral}px`
-      casa.style.textAlign = 'center'
-      linha.appendChild(casa)
     }
-    tabela.appendChild(linha)
-  }
-  document.body.appendChild(tabela)
+    return tabela;
 }
 
-function f_criarPeca(cor) {
-  let imagem = document.createElement('img')
-  imagem.setAttribute('src', `${cor}.png`)
-  imagem.classList.add('peca')
-  imagem.setAttribute('widht', `${tamanhoGeral - 5}px`)
-  imagem.setAttribute('height', `${tamanhoGeral - 5}px`)
-  imagem.id = `${pId++}`
-  imagem.addEventListener('drag', f_id)
-  imagem.style.userSelect = 'none'
-  imagem.style.margin = 0
-  return imagem
+function criaPeca(cor,ws) {
+    let imagem = document.createElement('img');
+		imagem.setAttribute('src', `img/${cor}.png`);
+		imagem.setAttribute('width', `${tamanhoCelula-4}px`);
+		imagem.setAttribute('height', `${tamanhoCelula-4}px`);
+		imagem.setAttribute('draggable','true');
+		imagem.setAttribute('id', ws);
+		imagem.setAttribute('class', cor);
+    return imagem;
 }
 
-function f_id(){
-  getId = this.getAttribute('id')
+function dragstart(){
+	document.addEventListener("dragstart", function(event) {
+	  event.dataTransfer.setData("Text", event.target.id);
+	  la = event.path[1].id;
+	  c = event.path[0].className;
+	  
+	});
 }
 
-
-function f_troca(){
-  const pecas = document.querySelectorAll('.peca')
-  //roda pelas peças e adiciona/remove drag
-  pecas.forEach((pecau) => {
-    pecau.draggable = !pecau.draggable
-  })
-}
-    
-function f_soltar(e) {
-  const peca = document.getElementById(`${getId}`)
-  peca.parentElement.addEventListener('dragover', f_over)
-  e.target.append(peca)
-  peca.parentElement.removeEventListener('dragover', f_over)
-  f_troca()
+function dragend() {
+	document.addEventListener("dragend", function(event) {
+	});
 }
 
-function f_over(e){
-  e.preventDefault()
-  const peca = document.getElementById(`${getId}`)
-
-  //pega as posições da colina e linha
-  const cOrigem = peca.parentElement.dataset.col 
-  const lOrigem = peca.parentElement.dataset.lin
-  const lDestino = e.target.dataset.lin 
-  const cDestino = e.target.dataset.col
-  
-  if ((peca.getAttribute('src') == 'red.png' && 
-    lDestino == lOrigem-1 || 
-    peca.getAttribute('src') == 'black.png' && 
-    lDestino-1 == lOrigem) &&
-    (cOrigem == cDestino-1 || cOrigem-1 == cDestino)) {
-    e.target.addEventListener('drop', f_soltar)
-    }
+function dragover() {
+	document.addEventListener("dragover", function(event) {
+	  event.preventDefault();
+	});
 }
+function drop(){
+	document.addEventListener("drop", function(event) {
+		event.preventDefault();
+		if ( event.target.className == "droptarget") {
+			const data = event.dataTransfer.getData("Text");
+				let w = event.path[0];
+				let e = w.childElementCount;
+				lf = event.target.id;
+				console.log(event)
+				if(e == '0' && la != lf && lf - la == 1 || lf - la == -1){
+					if(c == "red" && la > lf || c == "black" && la < lf)
+					event.target.appendChild(document.getElementById(data));
+				}
+		}
+	});
+}
+
+dragstart();
+dragend();
+dragover();
+drop();
